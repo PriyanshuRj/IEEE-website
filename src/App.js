ad45c9ce70e2ddcc288ad498952d12ac5ef32be6
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-06-26 02:57:04 +0530

    Updated Design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Author from "./components/Authors";
+import Hero from "./components/Hero";
+import Program from "./components/Programs";
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <Header/>
-      App
+      <Hero />
+      <About />
+      <Author />
+      <Program />
       <Footer/>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">IEE</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="/">About</Nav.Link>
+            <NavDropdown title="Programs" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="/">Action</NavDropdown.Item>
+              <NavDropdown.Item href="/">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#">
+              Authors
+            </Nav.Link>
+            <Nav.Link href="#">
+              Sponsors
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+

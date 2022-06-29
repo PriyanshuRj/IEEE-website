cd2717dbcede54c9b970460e60f82594279adba0
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-06-30 00:47:36 +0530

    Code refactored

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,55 @@
-import React from 'react';
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 import img from "../Images/ieee-logo.png";
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer() {
   return (
     <section className={styles.footer_section}>
-        <Container>
-            <Row>
-                <Col lg="12">
-                    <div className={styles.footer_text}>
-                        <div className={styles.ft_logo}>
-                            <a href="#" className={styles.footer_logo}><img src={img} alt="" / ></a>
-                        </div>
-                        <ul>
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Authors</a></li>
-                            <li><a href="#">Sponsors</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                        <div className={styles.ft_social}>
-                            <a href="#"><i className="bi bi-facebook"></i></a>
-                            <a href="#"><i className="bi bi-twitter"></i></a>
-                            <a href="#"><i className="bi bi-linkedin"></i></a>
-                            <a href="#"><i className="bi bi-instagram"></i></a>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col lg="12">
+            <div className={styles.footer_text}>
+              <div className={styles.ft_logo}>
+                <a href="#" className={styles.footer_logo}>
+                  <img src={img} alt="" />
+                </a>
+              </div>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Authors</a>
+                </li>
+                <li>
+                  <a href="#">Sponsors</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+              <div className={styles.ft_social}>
+                <a href="#">
+                  <i className="bi bi-facebook"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-twitter"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+                <a href="#">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
-  )
+  );
 }
--- src/components/SecondaryHero.js
+++ src/components/SecondaryHero.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import Styles from "../Styles/secondhero.module.css"
-export default function Hero({header}) {
-	console.log(header);
+import React from "react";
+import Styles from "../Styles/secondhero.module.css";
+export default function Hero({ header }) {
+  console.log(header);
   return (
-    <section className={Styles.textOverlay + " " + Styles.bannerTwo + " " + Styles.bgBannerTwo+ " " + Styles.overlayWhiteSlant }>
-	<div className="container">
-		<div className="row">
-			<div className="col-md-6" style={{zIndex:"100"}}>
-				
-				<div className={Styles.block} >
-					<h1>IEEE</h1>
-					<h2>Conference</h2>
-					<h3>2022</h3>
-					<h2>
-						{header}
-					</h2>
-					{/* <h6>15-17 December 2022</h6> */}
-					
-
-					
-				
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-  )
+    <section
+      className={
+        Styles.textOverlay +
+        " " +
+        Styles.bannerTwo +
+        " " +
+        Styles.bgBannerTwo +
+        " " +
+        Styles.overlayWhiteSlant
+      }
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{ zIndex: "100" }}>
+            <div className={Styles.block}>
+              <h1>IEEE</h1>
+              <h2>Conference</h2>
+              <h3>2022</h3>
+              <h2>{header}</h2>
+              {/* <h6>15-17 December 2022</h6> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }

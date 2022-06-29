1a7dfdb30a6ec90af15a83325a9591673543f638
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2022-06-29 15:55:56 +0530

    Contact us Updated

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,47 +1,38 @@
+
 import React from "react";
 import styles from "./ContactUs.module.css";
-
+// import { FaHeart} from 'react-icons/fa';
 export default function ContactUs() {
   return (
-    <div className="contactUs">
-        <div className={styles.background}>
-      <div className={styles.container}>
-        <div className={styles.screen}>
-          <div className={styles.screen_body}>
-            <div className={styles.screen_body_item}>
-              <div className={styles.app_title}>
-                <span>CONTACT</span>
-                <span>US</span>
-              </div>
-              <div className={styles.app_contact}>CONTACT INFO : +62 81 314 928 595</div>
-            </div>
-            <div className={styles.screen_body_item}>
-              <div className={styles.app_form}>
-                <div className={styles.app_form_group}>
-                  <input
-                    className={styles.app_form_control}
-                    placeholder="NAME"
-                  />
-                </div>
-                <div className={styles.app_form_group}>
-                  <input className={styles.app_form_control} placeholder="EMAIL" />
-                </div>
-                <div className={styles.app_form_group}>
-                  <input className={styles.app_form_control} placeholder="CONTACT NO" />
-                </div>
-                <div className={styles.message}>
-                  <input className={styles.app_form_control} placeholder="MESSAGE" />
-                </div>
-                <div className={styles.buttons}>
-                  <button className={styles.app_form_button}>CANCEL</button>
-                  <button className={styles.app_form_button}>SEND</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className = {styles.outerBody}>
+      <div className={styles.blurredBg}></div>
+	<div className={styles.contactUs}>
+		<form className={styles.contactForm}>
+			<div className={styles.title}>Contact Us</div>
+			<div className={styles.field} tabindex="1">
+				<label htmlFor="username" className={styles.contactLabel}>
+					<i className="fa fa-user"></i>Your Name
+				</label>
+				<input name="username" type="text" placeholder="e.g. john doe" className={styles.contactInput} required/>
+			</div>
+			<div className={styles.field} tabindex="2">
+				<label htmlFor="email" className={styles.contactLabel}>
+					<i className="fa fa-envelope"></i>Your Email
+				</label>
+				<input name="email" type="text" placeholder="email@domain.com" className={styles.contactInput} required/>
+			</div>
+			<div className={styles.field} tabindex="3">
+				<label htmlFor="message" className={styles.contactLabel}>
+        <i className="fa fa-edit"></i>Your Message
+				</label>
+				<textarea name="message" placeholder="type here" className={styles.contactTextarea}required></textarea>
+			</div>
+			<button type="reset">Send</button>
+			
+		</form>
+	</div>
     </div>
   );
 }
+
+

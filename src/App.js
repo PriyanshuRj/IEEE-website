0bc91bc702a6c094f82b6daf2548894c89c8eea5
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2022-06-30 14:01:44 +0530

    Navbar Updated

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Organizers from "./pages/Organizers";
-import History from "./pages/History";
 import TrackChair from "./pages/TrackChair";
-import Policies from "./pages/Policies";
 import Program from "./pages/Program";
 import Tutorial from "./pages/Tutorial";
 import KeyNote from "./pages/KeyNote";
 import OtherComitee from "./pages/OtherComitee";
 import TechnicalCommitee from "./pages/TechnicalCommitee";
 import Author from "./pages/Author";
+import SponsersOppurtunities from "./pages/SponsersOppurtunities";
+import CurrentSponsers from "./pages/CurrentSponsers";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Fragment } from "react";
@@ -23,15 +23,15 @@ function App() {
         <Routes>
           <Route esact path="/" element={<Home />}></Route>
           <Route exact path="/organizer" element={<Organizers />} />
-          <Route exact path="/history" element={<History />} />
           <Route exact path="/trackchair" element={<TrackChair />} />
-          <Route exact path="/polices" element={<Policies />} />
           <Route exact path="/keynote" element={<KeyNote />} />
           <Route exact path="/tutorial" element={<Tutorial />} />
           <Route exact path="/program" element={<Program />} />
           <Route exact path="/technical" element={<TechnicalCommitee />} />
           <Route exact path="/other" element={<OtherComitee />} />
           <Route exact path="/author" element={<Author />} />
+          <Route exact path = "/sponsersOppurtunities" element={<SponsersOppurtunities />} />
+          <Route exact path = "/currentSponsers" element={<CurrentSponsers />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/History.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function History() {
-  return (
-    <div>History</div>
-  )
-}

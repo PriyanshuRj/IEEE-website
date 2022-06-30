09f60097edda919d9d22521689864238017b8e62
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2022-06-30 19:59:25 +0530

    Author page again Updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import SponsersOppurtunities from "./pages/SponsersOppurtunities";
 import CurrentSponsers from "./pages/CurrentSponsers";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
---
+++ src/components/UI/Table.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+const TablePage=(props)=>{
+    const data_people ={
+        columns:[
+            {
+                label: 'Name',
+                field: 'name',
+                sort: 'asc'
+            },
+            {
+                label: 'Date',
+                field: 'date',
+                sort: 'asc'
+              },
+        ],
+        rows:[
+            {
+                'name':'Special Session/ Workshop Proposal due:',
+                'date':'31st August 2022'
+            },
+            {
+                'name':'Regular Paper Submission due:',
+                'date':'31st October 2022'
+            },
+            {
+                'name':'Paper Acceptance Date',
+                'date':'15th February 2023'
+            },
+            {
+                'name':'Camera-Ready Paper Submission due:',
+                'date':'15th March 2023'
+            },
+            {
+                'name':'Early Registration due:',
+                'date':'15th March 2022'
+            },
+            {
+                'name':'Late Registration Due',
+                'date':'1st April 2023'
+            },
+        ]
+    };
+return(
+    <MDBTable autoWidth striped>
+      <MDBTableBody rows={data_people.rows}/>
+    </MDBTable>
+  );
+};
+
+export default TablePage;

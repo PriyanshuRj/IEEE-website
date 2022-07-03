18e7d739671c8626774d2fd36ad5d01ff83be4f0
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-07-03 16:37:06 +0530

    Added Header links

--- src/components/UpperHeader.js
+++ src/components/UpperHeader.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome} from '@fortawesome/free-solid-svg-icons'
 export default function UpperHeader() {
   return (
     <div>
-      <Navbar bg="dark" expand="lg" variant="dark">
+      <Navbar bg="dark" expand="lg" variant="dark" style={{padding :"0px"}}>
   <Container>
-    <Navbar.Brand href="#home">IEEE.org</Navbar.Brand>
+    <Navbar.Brand href="https://www.ieee.org/">
+      <FontAwesomeIcon icon={faHome} height={16} width={16} className="mx-2" />
+      IEEE.org</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto text-white">
-        <Nav.Link href="#home" className="text-white">IEEE <em>Xplore</em> Digital Library</Nav.Link>
-        <Nav.Link href="#link" className="text-white">IEEE Standards</Nav.Link>
-        <Nav.Link href="#link" className="text-white">IEEE Spectrum</Nav.Link>
-        <Nav.Link href="#link" className="text-white">More Sites</Nav.Link>
+        <Nav.Link href="https://ieeexplore.ieee.org/Xplore/home.jsp" className="text-white">IEEE <em>Xplore</em> Digital Library</Nav.Link>
+        <Nav.Link href="https://standards.ieee.org/" className="text-white">IEEE Standards</Nav.Link>
+        <Nav.Link href="https://spectrum.ieee.org/" className="text-white">IEEE Spectrum</Nav.Link>
+        <Nav.Link href="https://www.ieee.org/sitemap.html" className="text-white">More Sites</Nav.Link>
         
       </Nav>
     </Navbar.Collapse>
